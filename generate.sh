db80c1566f4986bdf0502c522af79515c577ce13
Jared Bunting <jared.bunting@digitalreasoning.com>
2013-09-12 21:59:45 -0500

    PAGI-18 -- reorganization of files

--- generate.sh
+++ generate.sh
@@ -7,17 +7,19 @@ mkdir -p $OUTDIR
 OUTFILE=$OUTDIR/pagi.html
 TEMPMD=$OUTDIR/pagi.tmp.md
 
-cat $BASEDIR/header.htmlf > $OUTFILE
+cat $BASEDIR/web/header.htmlf > $OUTFILE
 
 sed -e "/<<<<pagis.xsd>>>>/ {
-	r pagis.xsd
+	r $BASEDIR/resources/pagis.xsd
 	d
 }" -e "/<<<<pagif-xml.xsd>>>>/ {
-	r pagif-xml.xsd
+	r $BASEDIR/resources/pagif-xml.xsd
 	d
 }" < $BASEDIR/pagi.md > $TEMPMD
 
 markdown_py -x codehilite -x extra $TEMPMD >> $BASEDIR/$OUTFILE
 
-cat $BASEDIR/footer.htmlf >> $OUTFILE
+cat $BASEDIR/web/footer.htmlf >> $OUTFILE
+
+cp $BASEDIR/assets/* $OUTDIR
 
